25cbcfb31a934844f52bea2520d32c6de2931999
Martin Buchwald <martin.buchwald@mulesoft.com>
2019-11-17 17:34:01 -0300

    tests

---
+++ integration/src/test/java/org/mule/test/routing/WhileTestCase.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.routing;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.rules.ExpectedException.none;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mule.runtime.api.exception.MuleException;
+import org.mule.runtime.core.api.event.CoreEvent;
+import org.mule.runtime.core.api.processor.Processor;
+import org.mule.test.AbstractIntegrationTestCase;
+
+
+public class WhileTestCase extends AbstractIntegrationTestCase {
+
+  @Rule
+  public ExpectedException expectedException = none();
+
+  @Override
+  protected String getConfigFile() {
+    return "while-config.xml";
+  }
+
+  @Test
+  public void noExecution() throws Exception {
+    flowRunner("basicNoExecution").run();
+  }
+
+  @Test
+  public void oneExecution() throws Exception {
+    flowRunner("basicOneExecution").run();
+  }
+
+  private int factorial(int n) {
+    if (n == 0) return 1;
+    return n * factorial(n - 1);
+  }
+
+  @Test
+  public void manyExecutions() throws Exception {
+    CoreEvent event = flowRunner("manyExecutions").withPayload(10).run();
+    assertThat(event.getMessage().getPayload().getValue(), is(factorial(10)));
+  }
+
+  @Test
+  public void volumeExecutions() throws Exception {
+    flowRunner("volumeExecutions").run();
+  }
+
+  @Test
+  public void errorExecution() throws Exception {
+    flowRunner("error").run();
+  }
+}
